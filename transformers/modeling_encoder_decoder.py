96916cdd78f790fd25dbb6839b5642461cd58136
Zhuoyu Wei <zhuwe@microsoft.com>
2019-11-27 15:31:03 +0800

    debug 1127 6

--- transformers/modeling_encoder_decoder.py
+++ transformers/modeling_encoder_decoder.py
@@ -369,8 +369,12 @@ class Model2Model(PreTrainedEncoderDecoder):
             kwargs_decoder["attention_mask"]=produced_decoder_attn_mask
             decoder_outputs = self.decoder(decoder_input_ids, **kwargs_decoder)
             decoder_ids=decoder_outputs[0].argmax(dim=-1)
-            print(decoder_ids)
             decoder_ids=decoder_ids[:,step]
+            print('decoder_input_ids shape = {}'.format(decoder_input_ids.size()))
+            print('decoder_output_ids shape = {}'.format(decoder_ids.size()))
+            print(decoder_input_ids[:,step+1])
+            print('########################################')
+            print(decoder_ids)
             decoder_input_ids[:,step+1]=decoder_ids
 
 
