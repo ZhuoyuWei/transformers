8e30f589851c809dd065a24c641305a0d782b23a
Zhuoyu Wei <zhuwe@microsoft.com>
2019-11-27 17:31:05 +0800

    test 1 layer decoder with pretrained init

--- examples/run_seq2seq.py
+++ examples/run_seq2seq.py
@@ -514,8 +514,9 @@ def main():
     # Load pretrained model and tokenizer. The decoder's weights are randomly initialized.
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
     config = BertConfig.from_pretrained(args.model_name_or_path)
+    config.num_hidden_layers=1
     #decoder_model = BertForMaskedLM(config)
-    decoder_model = BertForMaskedLM.from_pretrained(args.model_name_or_path)
+    decoder_model = BertForMaskedLM.from_pretrained(r'/data/zhuoyu/semantic_parsing/models')
     model = Model2Model.from_pretrained(
         args.model_name_or_path, decoder_model=decoder_model
     )
