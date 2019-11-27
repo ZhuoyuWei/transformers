6648c972bf03c6200c9ccd9652d7ec4ba7fe71b9
Zhuoyu Wei <zhuwe@microsoft.com>
2019-11-27 15:15:31 +0800

    debug 1127 4

--- scripts/train_seq2seq.sh
+++ scripts/train_seq2seq.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-CUDA_VISIBLE_DEVICES=0 python ../examples/run_seq2seq.py --data_dir ../../../data --output_dir ../../../output_test_3_bert \
---do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True --trained_checkpoints ../../../output_test_3_bert
+python ../examples/run_seq2seq.py --data_dir ../../../data --output_dir ../../../output_test_4_bert \
+--do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True --trained_checkpoints ../../../output_test_4_bert
