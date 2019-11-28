09fcc596b0455b3afb33aefbdd9cde6f2241bf8e
Zhuoyu Wei <zhuwe@microsoft.com>
2019-11-28 17:04:51 +0800

    update scripts

--- scripts/decoding_traing.sh
+++ scripts/decoding_traing.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-python ../examples/run_seq2seq.py --data_dir ../../../data --output_dir ../../../output_test_4_bert \
---do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True --trained_checkpoints ../../../output_test_4_bert --decoding_type=decoding
+python ../examples/run_seq2seq.py --data_dir ../../../datas/data_decoding --output_dir ../../../output_test_7_bert_decoding \
+--do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True --trained_checkpoints ../../../output_test_7_bert_decoding --decoding_type=decoding --num_train_epochs=50
+ecoding
--- scripts/train_seq2seq.sh
+++ scripts/train_seq2seq.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python ../examples/run_seq2seq.py --data_dir ../../../data --output_dir ../../../output_test_4_bert \
---do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True --trained_checkpoints ../../../output_test_4_bert
+python ../examples/run_seq2seq.py --data_dir ../../../datas/data_ori --output_dir ../../../output_test_7_bert_origin \
+--do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True --trained_checkpoints ../../../output_test_7_bert_origin --num_train_epochs=50
