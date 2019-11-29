172864fab8933e9a60cc3259d12b9ee71bc03820
Zhuoyu Wei <zhuwe@microsoft.com>
2019-11-29 10:39:01 +0800

    stable at 1129

--- scripts/decoding_traing.sh
+++ scripts/decoding_traing.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 python ../examples/run_seq2seq.py --data_dir ../../../datas/data_decoding --output_dir ../../../output_test_7_bert_decoding \
---do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True --trained_checkpoints ../../../output_test_7_bert_decoding --decoding_type=decoding --num_train_epochs=50
+--do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True --trained_checkpoints ../../../output_test_7_bert_decoding --decoding_type=decoding --num_train_epochs=500
--- scripts/train_seq2seq.sh
+++ scripts/train_seq2seq.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 python ../examples/run_seq2seq.py --data_dir ../../../datas/data_ori --output_dir ../../../output_test_7_bert_origin \
---do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True --trained_checkpoints ../../../output_test_7_bert_origin --num_train_epochs=50
+--do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True --trained_checkpoints ../../../output_test_7_bert_origin --num_train_epochs=500
