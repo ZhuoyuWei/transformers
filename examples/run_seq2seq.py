b29d9418f84b35a1fe714bad0fed21a418f418b9
Zhuoyu Wei <zhuwe@microsoft.com>
2019-12-02 15:47:35 +0800

    set is_decoder before create the model|3

--- examples/run_seq2seq.py
+++ examples/run_seq2seq.py
@@ -546,7 +546,7 @@ def main():
     # Load pretrained model and tokenizer. The decoder's weights are randomly initialized.
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
     config = BertConfig.from_pretrained(args.model_name_or_path)
-    config.num_hidden_layers=1
+    config.num_hidden_layers=3
     config.is_decoder=True
     decoder_model = BertForMaskedLM(config)
     #decoder_model = BertForMaskedLM.from_pretrained(r'/data/zhuoyu/semantic_parsing/models')
