91f86d7697b6f32ee25ed80df0b50fedc605f584
Zhuoyu Wei <zhuwe@microsoft.com>
2019-12-09 16:47:45 +0800

    add chemistry training

---
+++ scripts/chemistry_training.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python ../examples/run_chemistry_parser.py --data_dir ../../../datas/data_decoding --output_dir ../../../output_test_7_bert_decoding \
+--do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True --trained_checkpoints ../../../output_test_7_bert_decoding --decoding_type=decoding --num_train_epochs=500
