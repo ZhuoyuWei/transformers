edad26243f23de3d558971695bdf3a977ece12aa
Zhuoyu Wei <zhuwe@microsoft.com>
2020-01-06 15:32:41 +0800

    debug 0106 16

--- examples/run_chemistry_parser_v2.py
+++ examples/run_chemistry_parser_v2.py
@@ -105,6 +105,7 @@ def collate(data, tokenizer, input_block_size,output_block_size):
             output=tokenizer.build_inputs_with_special_tokens([])
         output=fit_to_block_size(output, output_block_size, tokenizer.pad_token_id)
         outputs.append(output)
+        print('debug output={}'.format(example.output))
         print('debug output={}'.format(output))
 
         if example.vocab_indexes is not None:
