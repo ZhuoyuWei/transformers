5444ab1a1ddc52f8872a10d7723bf3916c14b942
Zhuoyu Wei <zhuwe@microsoft.com>
2020-01-06 16:03:14 +0800

    debug 0106 24

--- scripts/chemistry_v2.sh
+++ scripts/chemistry_v2.sh
@@ -2,4 +2,4 @@
 
 python ../examples/run_chemistry_parser_v2.py --data_dir ../../../data --output_dir ../../../output_bert_decoding --output_block_size=128 \
 --do_train=True --per_gpu_train_batch_size=8 --do_evaluate=False --trained_checkpoints ../../../output_bert_decoding --decoding_type=pnt --num_train_epochs=$1 \
---decoder_version=v2 --encoder_model_name_or_path=/data/zhuoyu/semantic_parsing_v2/models
+--decoder_version=v2 --encoder_model_name_or_path=/data/zhuoyu/semantic_parsing_v2/bertmodels --decoder_model_name_or_path=/data/zhuoyu/semantic_parsing_v2/bertmodels
