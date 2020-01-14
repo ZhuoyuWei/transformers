37ed6becf358397c4ff6ca530228c1f991f46e4a
Zhuoyu Wei <zhuwe@microsoft.com>
2020-01-14 11:21:25 +0800

    add inf script

---
+++ scripts/chemistry_v2.test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+python ../examples/run_chemistry_parser_v2.py --data_dir ../../../data --output_dir ../../../output_bert_decoding --output_block_size=128 \
+--do_train=False --per_gpu_train_batch_size=8 --do_evaluate=False --trained_checkpoints ../../../output_bert_decoding --decoding_type=pnt  \
+--decoder_version=v2 --encoder_model_name_or_path=../../../bertmodels/encoder \
+--decoder_model_name_or_path=../../../bertmodels/decoder  --decoding_type=decoding
