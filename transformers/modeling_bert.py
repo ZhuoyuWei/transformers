d7999ccbcee4649a00c811eca986fea7606e5cdd
Zhuoyu Wei <zhuwe@microsoft.com>
2020-01-15 11:15:42 +0800

    debug at 0115 2

--- transformers/modeling_bert.py
+++ transformers/modeling_bert.py
@@ -1783,21 +1783,21 @@ class BertForMaskedLMVocabMask(BertForMaskedLM):
             #print('debug encoder_attention_mask {}'.format(encoder_attention_mask.size()))
             #print('debug encoder_attention_mask {}'.format(encoder_attention_mask))
 
-            print('##############################encoder_attention_mask##################################')
-            torch.set_printoptions(profile="full")
-            print(encoder_attention_mask)
-            torch.set_printoptions(profile="default")
+            #print('##############################encoder_attention_mask##################################')
+            #torch.set_printoptions(profile="full")
+            #print(encoder_attention_mask)
+            #torch.set_printoptions(profile="default")
 
             pointer_scores_mask=encoder_attention_mask.unsqueeze(dim=1)
             pointer_scores_mask=pointer_scores_mask.repeat([1,prediction_scores[1].size()[1],1])
             pointer_scores_mask=(1.0 - pointer_scores_mask) * -10000.0
             #print('pointer_scores_mask {}'.format(pointer_scores_mask))
-            print('pointer_scores_mask size = {}'.format(pointer_scores_mask.size()))
-            print('prediction_scores[1] size = {}'.format(prediction_scores[1].size()))
-            print('##############################POINTERERERERERs##################################')
-            torch.set_printoptions(profile="full")
-            print(pointer_scores_mask)
-            torch.set_printoptions(profile="default")
+            #print('pointer_scores_mask size = {}'.format(pointer_scores_mask.size()))
+            #print('prediction_scores[1] size = {}'.format(prediction_scores[1].size()))
+            #print('##############################POINTERERERERERs##################################')
+            #torch.set_printoptions(profile="full")
+            #print(pointer_scores_mask)
+            #torch.set_printoptions(profile="default")
 
             exit(-1)
             prediction_scores[1]=prediction_scores[1]+pointer_scores_mask
