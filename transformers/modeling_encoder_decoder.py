e6752e010b1d5e5a2e9cb9ddd2e0a559d974140a
Zhuoyu Wei <zhuwe@microsoft.com>
2020-01-16 15:46:13 +0800

    debug at 0116 3

--- transformers/modeling_encoder_decoder.py
+++ transformers/modeling_encoder_decoder.py
@@ -506,8 +506,6 @@ class Model2Model(PreTrainedEncoderDecoder):
             print('Debug decoder_input_ids ###########################')
             print('decoder_input_ids size {}'.format(decoder_input_ids.size()))
             vocab_mask_index=self._get_vocab_index_by_decoder_input_ids(decoder_input_ids,tokenizer,fsa)
-            if step == 10:
-                exit(-1)
             kwargs_decoder["attention_mask"]=produced_decoder_attn_mask
             kwargs_decoder["vocab_mask_index"]=vocab_mask_index
             decoder_outputs = self.decoder(decoder_input_ids, **kwargs_decoder)
@@ -544,8 +542,12 @@ class Model2Model(PreTrainedEncoderDecoder):
             decoder_input_ids=torch.cat([decoder_input_ids,decoder_ids],dim=1)
             #print('decoder input ids:')
             #print(decoder_input_ids)
-
-
+            print('########################decoder_input_ids##############################')
+            print('decoder_input_ids size {}'.format(decoder_input_ids.size()))
+            torch.set_printoptions(profile="full")
+            print('{}'.format(decoder_input_ids))
+            torch.set_printoptions(profile="default")
+        exit(-1)
         return decoder_input_ids,vocab_mask_index
 
 
