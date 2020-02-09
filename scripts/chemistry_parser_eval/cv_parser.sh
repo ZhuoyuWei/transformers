c4605d32656832cffd555dcbe0423706c654619b
zhuwe <zhuwe@microsoft.com>
2020-02-09 16:39:16 +0800

    add 10 cv|fix bug 8

--- scripts/chemistry_parser_eval/cv_parser.sh
+++ scripts/chemistry_parser_eval/cv_parser.sh
@@ -44,8 +44,8 @@ for k in $( seq 0 9 )
 do
    python ../../examples/run_chemistry_parser_v2.py --data_dir $DATA_DIR/$k --output_dir $OUTPUT_DIR/${k}_bert_output --output_block_size=128 \
           --do_train=True --per_gpu_train_batch_size=8 --do_evaluate=True  --num_train_epochs=$TRAIN_EPOCH \
-          --decoder_version=v2 --encoder_model_name_or_path=$MODEL/encoder \
-          --decoder_model_name_or_path=$MODEL/decoder --encoder_lr=$LR --decoder_lr=$LR \
+          --decoder_version=v2 --encoder_model_name_or_path=$MODEL_DIR/encoder \
+          --decoder_model_name_or_path=$MODEL_DIR/decoder --encoder_lr=$LR --decoder_lr=$LR \
           --decoding_type=decoding
    python scorer.py $DATA_DIR/$k/dev.ans $OUTPUT_DIR/${k}_bert_output/dev.res >> $OUTPUT_DIR/log
 done
